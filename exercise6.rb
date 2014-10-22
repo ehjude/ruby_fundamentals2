0a07fc549be9668a0e180e3ea616796667719fbb
Jude Fiorillo <jude.fiorillo@gmail.com>
2014-10-22 16:13:28 -0400

    create grocery list app

--- exercise6.rb
+++ exercise6.rb
@@ -1,27 +1,29 @@
-$grocery_list = ["quinoa", "brocoli", "eggs", "almonds"]	
+grocery_list = ["quinoa", "brocoli", "salmon", "almonds"]	
 
-def current_list	
+def current_list(groceries)	
 	puts "Your current grocery list is:" 
 	
-	$grocery_list.each do |item|
+	groceries.each do |item|
 		puts "* #{item}"		
 	end
 end
 
-current_list
+current_list(grocery_list)
 
-puts "There are #{$grocery_list.length} items on this list"
+puts "There are #{grocery_list.length} items on this list"
 
-if $grocery_list.include?("bananas")
+if grocery_list.include?("bananas")
 	puts "Yeaaa, bananas are awesome"
 else 
 	puts "Get some bananas fool!"
 end
 
-puts "The second item on the list is $grocery_list[1]"
-# this doesn't seem to be working
+puts "The second item on the list is #{grocery_list[1]}"
 
 puts "Do you want to add any items?"
-$grocery_list << gets.chomp
+grocery_list << gets.chomp
 
-puts "Your new, alphabetized list is #{current_list}"
+puts "Btw, salmon sucks, let's delete that"
+grocery_list.delete("salmon")
+
+puts "Your new, alphabetized list, without salmon, is #{current_list(grocery_list).sort}"
