6586e2a0dd047980dd59d9b89df3f9bb944198e5
Jude Fiorillo <jude.fiorillo@gmail.com>
2014-10-22 15:43:50 -0400

    check for bananas on grocery list

--- exercise6.rb
+++ exercise6.rb
@@ -2,6 +2,7 @@ $grocery_list = ["quinoa", "brocoli", "eggs", "almonds"]
 
 def current_list	
 	puts "Your current grocery list is:" 
+	
 	$grocery_list.each do |item|
 		puts "* #{item}"		
 	end
@@ -9,6 +10,18 @@ end
 
 current_list
 
+puts "There are #{$grocery_list.length} items on this list"
+
+if $grocery_list.include?("bananas")
+	puts "Yeaaa, bananas are awesome"
+else 
+	puts "Get some bananas fool!"
+end
+
+puts "The second item on the list is $grocery_list[1]"
+# this doesn't seem to be working
+
 puts "Do you want to add any items?"
 $grocery_list << gets.chomp
-current_list
+
+puts "Your new, alphabetized list is #{current_list}"
